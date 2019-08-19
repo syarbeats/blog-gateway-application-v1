30c8bb9cc856e7c45691fd6bb3b7991cbdfff8be
Syarif Hidayat <srf.hidayat@gmail.com>
2019-08-19 08:58:37 +0700

    first commit

---
+++ src/main/java/com/mitrais/cdc/bloggatewayapplicationv1/BlogGatewayApplicationV1Application.java
@@ -0,0 +1,13 @@
+package com.mitrais.cdc.bloggatewayapplicationv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogGatewayApplicationV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogGatewayApplicationV1Application.class, args);
+    }
+
+}
