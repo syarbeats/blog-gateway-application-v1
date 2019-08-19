1789f94133f5b45fe45c9d80fe69a3701e260207
Syarif Hidayat <srf.hidayat@gmail.com>
2019-08-19 09:35:48 +0700

    add zuul routing configuration for blog microservices

--- src/main/java/com/mitrais/cdc/bloggatewayapplicationv1/BlogGatewayApplicationV1Application.java
+++ src/main/java/com/mitrais/cdc/bloggatewayapplicationv1/BlogGatewayApplicationV1Application.java
@@ -2,8 +2,10 @@ package com.mitrais.cdc.bloggatewayapplicationv1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class BlogGatewayApplicationV1Application {
 
     public static void main(String[] args) {
