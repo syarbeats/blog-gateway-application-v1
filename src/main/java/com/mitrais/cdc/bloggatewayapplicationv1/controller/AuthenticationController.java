e73071d6e004df3cef93a38b795cfcd95b3581e3
Syarif Hidayat <srf.hidayat@gmail.com>
2019-08-20 10:19:53 +0700

    Add json web token functionality.

--- src/main/java/com/mitrais/cdc/bloggatewayapplicationv1/controller/AuthenticationController.java
+++ src/main/java/com/mitrais/cdc/bloggatewayapplicationv1/controller/AuthenticationController.java
@@ -1,6 +1,9 @@
 package com.mitrais.cdc.bloggatewayapplicationv1.controller;
 
+import com.mitrais.cdc.bloggatewayapplicationv1.payload.APIResponse;
 import com.mitrais.cdc.bloggatewayapplicationv1.payload.AuthenticationPayload;
+import com.mitrais.cdc.bloggatewayapplicationv1.payload.LoginResponse;
+import com.mitrais.cdc.bloggatewayapplicationv1.payload.TokenPayload;
 import com.mitrais.cdc.bloggatewayapplicationv1.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -19,9 +25,21 @@ public class AuthenticationController {
     @PostMapping("/authentication")
     public ResponseEntity<?> Login(@RequestBody AuthenticationPayload authenticationPayload){
 
-        if(!authenticationService.Login(authenticationPayload)){
-            return ResponseEntity.ok("Authentication is failed");
+        Map<Object, Object> data = new HashMap<>();
+        LoginResponse response = authenticationService.Login(authenticationPayload);
+        String token = response.getData().getToken();
+        String username = response.getData().getUsername();
+
+        if(!response.isSuccess()){
+            data.put("status", false);
+            data.put("message", "Authentication is failed");
+            data.put("token", "");
+            return ResponseEntity.ok(data);
         }
-        return ResponseEntity.ok("Authentication is success");
+        data.put("status", true);
+        data.put("message", "You have login successfully");
+        data.put("token", token);
+
+        return ResponseEntity.ok(data);
     }
 }
